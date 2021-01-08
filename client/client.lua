a78248644d5b692f36aa2cfb3786d3f45e6fda3f
iWanheda <lexihvh@gmail.com>
2021-01-08 23:52:27 +0000

    Uploaded Updated Version

--- client/client.lua
+++ client/client.lua
@@ -1,15 +1,40 @@
+local enableWaypoints = true
+
 Citizen.CreateThread( function()
 	while true do
 		Citizen.Wait( 4 )
-		local waypointFound = GetFirstBlipInfoId( 8 )
+		
+		if ( enableWaypoints ) then
+			local waypointFound = GetFirstBlipInfoId( 8 )
 
-		if DoesBlipExist( waypointFound ) then
-			local coords = GetBlipCoords( waypointFound )
+			if DoesBlipExist( waypointFound ) then
+				local coords = GetBlipCoords( waypointFound )
+				local blipHeight = Config.ScaleWithScreen and ( #( GetEntityCoords( PlayerPedId() ) - coords ) ) * 0.5 or 100
 
-		    DrawMarker( 1, coords.x, coords.y, coords.z, 0, 0, 0, 0, 0, 0, 
-				6.0, 6.0, 1000.0, -- Size
-				Config.MarkerColor[1], Config.MarkerColor[2], Config.MarkerColor[3], -- Color
-			155, false, false, 2, false, false, false, false )
+				if ( blipHeight <= 30 ) then
+					blipHeight = 30
+				end
+
+				DrawMarker( 1, coords.x, coords.y, coords.z, 0, 0, 0, 0, 0, 0, 
+					8.0, 8.0, blipHeight, -- Size
+					Config.MarkerColor[1], Config.MarkerColor[2], Config.MarkerColor[3], -- Color
+				155, false, false, 2, false, false, false, false )
+
+				DrawMarker( 5, coords.x, coords.y, coords.z + blipHeight + 2.0, 0, 0, 0, 0, 0, 0, 
+					11.0, 11.0, 11.0, -- Size
+					Config.MarkerColor[1], Config.MarkerColor[2], Config.MarkerColor[3], -- Color
+				155, false, true, 2, false, false, false, false )
+			end
 		end
 	end
 end )
+
+RegisterCommand( 'waypoint', function()
+	enableWaypoints = not enableWaypoints
+
+	TriggerEvent( 'chat:addMessage', {
+		color = { 255, 0, 0 },
+		multiline = false,
+		args = { '^*Waypoints: ', 'You have ' .. ( enableWaypoints and '^2enabled' or '^1disabled' ) .. ' ^7Waypoints!' }
+	} )
+end, false )
--- client/config.lua
+++ client/config.lua
@@ -1,3 +1,5 @@
 Config = { }
 
 Config.MarkerColor = { 255, 125, 230 } -- Pink https://www.rapidtables.com/web/color/RGB_Color.html
+
+Config.ScaleWithScreen = true
