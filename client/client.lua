05351612de2cca899324270c3208e31c044133f1
iWanheda <lexihvh@gmail.com>
2021-01-09 00:16:34 +0000

    Updated

--- client/client.lua
+++ client/client.lua
@@ -1,6 +1,8 @@
-local enableWaypoints = true
+local enableWaypoints, blipClr = true, nil
 
 Citizen.CreateThread( function()
+	blipClr = Config.MarkerColor
+
 	while true do
 		Citizen.Wait( 4 )
 		
@@ -17,12 +19,12 @@ Citizen.CreateThread( function()
 
 				DrawMarker( 1, coords.x, coords.y, coords.z, 0, 0, 0, 0, 0, 0, 
 					8.0, 8.0, blipHeight, -- Size
-					Config.MarkerColor[1], Config.MarkerColor[2], Config.MarkerColor[3], -- Color
+					blipClr[1], blipClr[2], blipClr[3], -- Color
 				155, false, false, 2, false, false, false, false )
 
 				DrawMarker( 5, coords.x, coords.y, coords.z + blipHeight + 2.0, 0, 0, 0, 0, 0, 0, 
 					11.0, 11.0, 11.0, -- Size
-					Config.MarkerColor[1], Config.MarkerColor[2], Config.MarkerColor[3], -- Color
+					blipClr[1], blipClr[2], blipClr[3], -- Color
 				155, false, true, 2, false, false, false, false )
 			end
 		end
@@ -38,3 +40,35 @@ RegisterCommand( 'waypoint', function()
 		args = { '^*Waypoints: ', 'You have ' .. ( enableWaypoints and '^2enabled' or '^1disabled' ) .. ' ^7Waypoints!' }
 	} )
 end, false )
+
+RegisterCommand( 'wcolor', function( source, args, raw )
+	if ( args[1] ~= nil ) then
+		setBlipColor( args[1] )
+	else
+		TriggerEvent( 'chat:addMessage', {
+			color = { 255, 0, 0 },
+			multiline = false,
+			args = { '^*Waypoints: ', 'Correct usage is ^1/wcolor <color>' }
+		} )
+	end
+end, false )
+
+function setBlipColor( clr )
+	if ( tostring( clr ) == 'red' ) then
+		blipClr = { 255, 0, 0 }
+	elseif ( tostring( clr ) == 'green' ) then
+		blipClr = { 0, 255, 0 }
+	elseif ( tostring( clr ) == 'blue' ) then
+		blipClr = { 0, 0, 255 }
+	elseif ( tostring( clr ) == 'pink' ) then
+		blipClr = Config.MarkerColor
+	elseif ( tostring( clr ) == 'default' ) then
+		blipClr = Config.MarkerColor
+	else
+		TriggerEvent( 'chat:addMessage', {
+			color = { 255, 0, 0 },
+			multiline = false,
+			args = { '^*Waypoints: ', 'Invalid Color!' }
+		} )
+	end
+end
--- client/config.lua
+++ client/config.lua
@@ -2,4 +2,5 @@ Config = { }
 
 Config.MarkerColor = { 255, 125, 230 } -- Pink https://www.rapidtables.com/web/color/RGB_Color.html
 
+-- Setting this to false will help with CPU usage, almost nothing tho...
 Config.ScaleWithScreen = true
